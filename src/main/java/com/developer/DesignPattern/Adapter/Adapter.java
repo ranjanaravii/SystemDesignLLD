ee4b47d11a2109c89a86b34c66714e42df13dc56
raviranjankumar <raviranjan.kumar@lenskart.com>
2025-07-24 23:14:26 +0530

    adapter design patter

---
+++ src/main/java/com/developer/DesignPattern/Adapter/Adapter.java
@@ -0,0 +1,9 @@
+package com.developer.DesignPattern.Adapter;
+/*
+ * A constructor which adapts an existing interface X to conform to the required interface Y.
+ * Adapter is a structural design pattern that allows objects with incompatible interfaces to collaborate.
+ * It is used to make classes work together that couldn't otherwise because of incompatible interfaces.
+ * It is used to make existing classes work with others without modifying their source code.
+ */
+public class Adapter {
+}
