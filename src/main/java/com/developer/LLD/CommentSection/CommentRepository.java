59c91ca378838f121282197aa14520944f5ffec0
raviranjankumar <raviranjan.kumar@lenskart.com>
2025-07-26 00:14:33 +0530

    comment section

---
+++ src/main/java/com/developer/LLD/CommentSection/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.developer.LLD.CommentSection;
+
+import java.util.List;
+
+public interface CommentRepository {
+
+    void save(Comment comment);
+
+    Comment findById(String commentId);
+
+    List<Comment> findByParentId(String parentId);
+
+    void delete(String commentId);
+    
+    List<Comment> findByEntityId(String entityId); // e.g., post/article ID
+}
---
+++ src/main/java/com/developer/LLD/CommentSection/User.java
@@ -0,0 +1,19 @@
+package com.developer.LLD.CommentSection;
+
+public class User {
+    private String id;
+    private String username;
+
+    public String getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
